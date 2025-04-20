73eead5df937129ec2a6b4670577a52c19d775ff
IPineappleI <mmolshevskiy@edu.hse.ru>
2025-04-20 14:49:01 +0300

    remove unnecessary variable

--- app/routers/students.py
+++ app/routers/students.py
@@ -102,7 +102,7 @@ async def patch_student(student_id: uuid.UUID, student_data: schemas.StudentUpda
         student_data.level_id = None
 
     if student_data.terminated:
-        student_groups = db.query(models.StudentGroup).filter(models.StudentGroup.student_id == student_id).delete()
+        db.query(models.StudentGroup).filter(models.StudentGroup.student_id == student_id).delete()
 
     await patch_user(student.user_id, student_data, db)
 
